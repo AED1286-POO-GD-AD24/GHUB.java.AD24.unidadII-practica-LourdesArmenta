d2a7afc13938aee69375b5601843474479c0808e
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-29 17:00:56 +0000

    Actualizacion clase Alumno

---
+++ miPrincipal/Alumno.java
@@ -0,0 +1,23 @@
+package miPrincipal;
+public class Alumno{
+    //campos o atributo de la clase
+    int id;
+    String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private char genero;
+    private int edad;
+    //Metodo
+    public void mostrarNombre(){
+
+    }
+    public void mostrarEdad(){
+
+    }
+    public void saludo(){
+        System.out.println("Hola a todos, me da gusto conocerlos");
+
+    }
+
+    
+}
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -6,6 +6,32 @@ public class Principal {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        //System.out.println(new Principal().getGreeting());
+        //Creando un objeto de tipo Alumno
+        Alumno alumno1 = new Alumno();
+        Alumno alumno2 = new Alumno();
+        Alumno alumno3 = new Alumno();
+
+        alumno1.saludo(); //el alumno1 invoca al salido
+        alumno1.id = 123;
+        alumno1.nombre ="Juan";
+
+        alumno2.saludo(); //el alumno2 invoca al saludo
+        alumno2.id = 124;
+        alumno2.nombre ="Paco";
+
+        alumno3.saludo(); //el alumno3 invoca al salido
+        alumno3.id = 125;
+        alumno3.nombre = "Luis";
+
+        
+        System.out.println("El nombre del alumno 1 ="+alumno1.nombre);
+        System.out.println("El nombre del alumno 2 ="+alumno2.nombre);
+        System.out.println("El nombre del alumno 3 ="+alumno3.nombre);
+
+
+
+       
+
     }
 }
