6090786afe0d85a298f892934402ad5915c53490
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-30 16:58:36 +0000

    Creacion de clase Alumno

--- miPrincipal/Alumno.java
+++ miPrincipal/Alumno.java
@@ -1,12 +1,32 @@
 package miPrincipal;
 public class Alumno{
     //campos o atributo de la clase
-    int id;
-    String nombre;
+    private int id;
+    private String nombre;
     private String apellidoPaterno;
     private String apellidoMaterno;
     private char genero;
     private int edad;
+    //Metodo constructores
+    public Alumno(){
+        this.id = 0;
+        this.nombre = "";
+        this.apellidoMaterno="";
+        this.apellidoPaterno="";
+        this.genero ='X';
+        this.edad = 0;
+
+    }
+    public Alumno(int id, String nombre, String apellidoPaterno,
+                 String apellidoMaterno, char genero, int edad){
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidoPaterno = apellidoPaterno;
+            this.apellidoMaterno = apellidoMaterno;
+            this.genero = genero;
+            this.edad = edad;
+
+    }
     //Metodo
     public void mostrarNombre(){
 
@@ -18,6 +38,50 @@ public class Alumno{
         System.out.println("Hola a todos, me da gusto conocerlos");
 
     }
+
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno(){
+        return this.apellidoPaterno;
+    }
+    public void setApellidoPaterno(String apellidoPaterno){
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno(){
+        return this.apellidoMaterno;
+    }
+    public void setApellidoMaterno(String apellidoMaterno){
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    public char getGenero() {
+        return genero;
+    }
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+
+
+
 
     
 }
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -9,25 +9,35 @@ public class Principal {
         //System.out.println(new Principal().getGreeting());
         //Creando un objeto de tipo Alumno
         Alumno alumno1 = new Alumno();
-        Alumno alumno2 = new Alumno();
-        Alumno alumno3 = new Alumno();
+        Alumno alumno2 = new Alumno(123,"Lourdes","Armenta","Lindoro",'M',56);
+        Alumno alumno3 = new Alumno(123,"Pedro","Lopez","Perez",'H',18);
+        
 
         alumno1.saludo(); //el alumno1 invoca al salido
-        alumno1.id = 123;
-        alumno1.nombre ="Juan";
+        //alumno1.id = 123; //el id no es visible desde la clase Principal porque es privado
+        //alumno1.nombre ="Juan";
+        alumno1.setNombre("Lucia");
+        alumno1.setApellidoPaterno("Gallardo");
+        alumno1.setApellidoMaterno("Quiñonez");
+
+        System.out.println("Mi nombre es "+alumno1.getNombre());
 
         alumno2.saludo(); //el alumno2 invoca al saludo
-        alumno2.id = 124;
-        alumno2.nombre ="Paco";
+        System.out.println("Mi nombre completo es "+alumno2.getNombre()+" "+
+                     alumno2.getApellidoPaterno()+" "+alumno2.getApellidoMaterno());
+        //alumno2.id = 124;
+        //alumno2.nombre ="Paco";
 
         alumno3.saludo(); //el alumno3 invoca al salido
-        alumno3.id = 125;
-        alumno3.nombre = "Luis";
+        System.out.println("Mi nombre completo es "+alumno3.getNombre()+" "+
+                     alumno2.getApellidoPaterno()+" "+alumno3.getApellidoMaterno());
+        //alumno3.id = 125;
+        //alumno3.nombre = "Luis";
 
         
-        System.out.println("El nombre del alumno 1 ="+alumno1.nombre);
-        System.out.println("El nombre del alumno 2 ="+alumno2.nombre);
-        System.out.println("El nombre del alumno 3 ="+alumno3.nombre);
+        //System.out.println("El nombre del alumno 1 ="+alumno1.nombre);
+        //System.out.println("El nombre del alumno 2 ="+alumno2.nombre);
+        //System.out.println("El nombre del alumno 3 ="+alumno3.nombre);
 
 
 
