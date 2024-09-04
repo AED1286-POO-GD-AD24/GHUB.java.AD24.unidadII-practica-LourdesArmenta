be62cc67a5d4f8afc74c26e7e50fd56e83f40029
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-09-04 16:07:53 +0000

    Actualizacion electrodomesticos

--- miPrincipal/Animal.java
+++ miPrincipal/Animal.java
@@ -6,21 +6,61 @@ public class Animal {
     private int edad;
     private String especie;
     private String propietario;
+ 
 
     public Animal(){
 
     }
     public Animal(String genero, String nombre, int edad, String especie, String propietario){
-        genero = genero;
-        nombre = nombre;
-        edad = edad;
-        especie = especie;
-        propietario = propietario;
+        this.genero = genero;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especie = especie;
+        this.propietario = propietario;
 
     }
 
     public void obtenerNombreDueño(){
-        System.out.println(propietario);
+        System.out.println("El propietario = "+ propietario);
     }
+
+    public String getGenero(){
+        return this.genero;
+    }
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public String getEspecie(){
+        return this.especie;
+    }
+    public void setEspecie(String especie){
+        this.especie = especie;
+    }
+
+    public String getPropietario(){
+        return this.propietario;
+    }
+    public void setPropietario(String propietario){
+        this.propietario = propietario;
+
+    }
+
+
     
 }
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -50,6 +50,39 @@ public class Principal {
         System.out.println("El animal 2 su propietario es: ");
         animal2.obtenerNombreDueño();
 
+        animal1.setGenero("hembra");
+        animal1.setNombre("Fiona");
+        animal1.setEdad(1);
+        animal1.setEspecie("Perro");
+        animal1.setPropietario("Lourdes");
+
+        animal2.setGenero("hembra");
+        animal2.setPropietario("Pedro");
+
+        System.out.println("El animal 1 su propietario es: ");
+        animal1.obtenerNombreDueño();
+
+        System.out.println("El animal 2 su propietario es: ");
+        animal2.obtenerNombreDueño();
+        
+        System.out.println("Propietario de animal 1 "+animal1.getPropietario());
+        System.out.println("Genero de animal1 "+animal1.getGenero());
+        System.out.println("Nombre animal 1: "+animal1.getNombre());
+        System.out.println("Edad de animal 1: "+animal1.getEdad());
+        System.out.println("Especie de animal 1: "+animal1.getEspecie());
+
+        System.out.println("Propietario de animal 2 "+animal2.getPropietario());
+        System.out.println("Genero de animal 2 "+animal2.getGenero());
+        System.out.println("Nombre animal 2: "+animal2.getNombre());
+        System.out.println("Edad de animal 2: "+animal2.getEdad());
+        System.out.println("Especie de animal 2: "+animal2.getEspecie());
+
+
+
+
+
+
+
       
 
 
