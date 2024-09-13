386dde88d098d621a9b82299b56178ced7b20f85
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-09-13 16:54:45 +0000

    Actualizacion clase Edad

---
+++ miPrincipal/AppEdad.java
@@ -0,0 +1,37 @@
+package miPrincipal;
+import paquete1.Edad;
+
+public class AppEdad {
+    public static void main(String[] arg){
+        //probar la forma de crear objetos
+        Edad edad1 = new Edad();
+        edad1.asignarEdadHijo(12);
+        edad1.asignarEdadMadre(40);
+        edad1.asignarEdadPadre(42);
+        System.out.println("Edad del hijo: "+edad1.leerHijo());
+        System.out.println("Edad de la Madre:"+edad1.leerMadre());
+        System.out.println("Edad del Padre: "+edad1.leerPadre());
+
+        //crear un objeto edad através de sus constructores
+
+        Edad edad2 = new Edad(8,20,40);
+        System.out.println("Edad del hijo: "+edad2.leerHijo());
+        System.out.println("Edad de la Madre:"+edad2.leerMadre());
+        System.out.println("Edad del Padre: "+edad2.leerPadre());
+
+        //crear un objeto edad a traves de su constructor  sin argumentos
+        Edad edad3 = new Edad();
+        System.out.println("Edad del hijo: "+edad3.leerHijo());
+        System.out.println("Edad de la Madre:"+edad3.leerMadre());
+        System.out.println("Edad del Padre: "+edad3.leerPadre());
+
+
+
+        
+
+
+       
+
+    }
+    
+}
--- paquete1/Edad.java
+++ paquete1/Edad.java
@@ -8,42 +8,59 @@ public class Edad {
 
     //constructor por defecto
     public Edad(){
+        edadHijo = 0;
+        edadMadre = 0;
+        edadPadre =0;
 
     }
     //constructo 2
     public Edad(int edad1){
+        //pendientes
 
     }
     //constructir 3
     public Edad(int edad1, int edad2){
+        //pendiente
 
     }
     //constructor 4
     public Edad(int edad1, int edad2, int edad3){
+        edadHijo  = edad1;
+        edadMadre = edad2;
+        edadPadre = edad3;
 
     }
+    
+
 
     public void iniciar(){
 
     }
+    public int leerHijo(){ //get
 
-    public int leerHijo(){
+        return edadHijo;
+    }
 
-        return 0;
+    public void asignarEdadHijo(int edad){ //set
+        edadHijo = edad;
     }
 
     public int leerMadre(){
-        return 0;
+        
+        return edadMadre;
+    }
+    public void asignarEdadMadre(int edad){ //set
+        edadMadre=edad;
     }
 
     public int leerPadre(){
-        return 0;
+        return edadPadre;
     }
 
+    public void asignarEdadPadre(int edad){ //set
+        edadPadre = edad;
+    }
 
-
-
-   
 
     
 }
