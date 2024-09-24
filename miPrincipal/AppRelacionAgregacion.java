61804e02812d801c403875010d2d4aa3aaeedb06
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-09-24 16:47:47 +0000

    Cardinalidad de 1 a muchos entre Computadora y Monitor

--- miPrincipal/AppRelacionAgregacion.java
+++ miPrincipal/AppRelacionAgregacion.java
@@ -8,8 +8,19 @@ public class AppRelacionAgregacion {
         Computadora computadora = new Computadora("Apple","MacBook pro");
         computadora.obtenerDetalles();
         Monitor monitor = new Monitor("LG",16);
-        Computadora computadora2= new Computadora("HP","Mll50",monitor);
-        computadora2.obtenerDetalles();
+        computadora.setMonitor(monitor);
+        computadora.obtenerDetalles();
+
+        //instanciar una computadora con tres monitores
+        Computadora computadora2= new Computadora("HP","Mll50",3);
+        Monitor []arregloMonitores = new Monitor[3];
+        arregloMonitores[0]=new Monitor("HP",12);
+        arregloMonitores[1]=new Monitor("Acer",15);
+        arregloMonitores[2]=new Monitor("Mac",21);
+        computadora2.setMonitores(arregloMonitores);
+        
+
+
 
 
     }
--- paquete2/Computadora.java
+++ paquete2/Computadora.java
@@ -4,17 +4,40 @@ public class Computadora {
     private String marca;
     private String modelo;
     private Monitor monitor;
+    private Monitor monitor2;
+    private Monitor []  monitores;
+
     //
     public Computadora() {
+        this.marca="";
+        this.modelo ="";
+        this.monitor=null;
+        this.monitor2=null;
     }
     public Computadora(String marca, String modelo) {
         this.marca = marca;
         this.modelo = modelo;
+        this.monitor=null;
+        monitores = new Monitor[3];
     }
     public Computadora(String marca, String modelo, Monitor monitor) {
         this.marca = marca;
         this.modelo = modelo;
         this.monitor = monitor;
+    }
+
+    public Computadora(String marca, String modelo, Monitor monitor1, Monitor monitor2) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.monitor = monitor1;
+        this.monitor2 = monitor2;
+    }
+
+
+    public Computadora(String marca, String modelo, int n) {
+        this.marca = marca;
+        this.modelo = modelo;
+        monitores = new Monitor[n];
     }
     //getter y setter
     public String getMarca() {
@@ -35,12 +58,35 @@ public class Computadora {
     public void setMonitor(Monitor monitor) {
         this.monitor = monitor;
     }
+    
 
     public void obtenerDetalles(){
-
-       // System.out.println("Computadora: "+marca+" Modelo: "+modelo+" Monitor "+.obtenerDetalles());
+        if(monitor ==null)
+             System.out.println("Computadora: "+marca+" Modelo: "+modelo);
+        else
+             System.out.println("Computadora: "+marca+" Modelo: "+modelo+
+                        "Monitor Marca: "+monitor.getMarca()+" Monitor Tamaño:"+monitor.getTamanio());
 
     }
+    public Monitor getMonitor2() {
+        return monitor2;
+    }
+    public void setMonitor2(Monitor monitor2) {
+        this.monitor2 = monitor2;
+    }
+
+    //el metodo get,regresa un arreglo de monitores
+    public Monitor[] getMonitores() {
+        return monitores;
+    }
+
+    //el método set asigna un arreglo de monitores
+    public void setMonitores(Monitor[] monitores) {
+        this.monitores = monitores;
+    }
+    
+
+
     
     
     
