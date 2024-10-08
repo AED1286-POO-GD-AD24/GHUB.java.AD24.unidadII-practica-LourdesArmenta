3159e25461283acec48362f252ba5a2242b8466d
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-10-08 23:52:02 +0000

    Metodo Finallize

--- metodoFinalice/ProbarDemo.java
+++ metodoFinalice/ProbarDemo.java
@@ -1,7 +1,7 @@
 package metodoFinalice;
 
 public class ProbarDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
        
         Demo d1,d2;//declaracion de dos referencias de tipo Demo
         Prueba p1,p2;//declaracion de dos referencias de tipo Prueba
@@ -9,13 +9,16 @@ public class ProbarDemo {
         p1 = new Prueba();//creacion de un objeto de tipo Prueba
         System.gc(); //no se libera ningun objeto
         p2=p1;//p2 referencia al mismo objeto que p1
-        p1 =new Prueba();//se crea un nuevo objeto de tipo Prueba
-        System.gc(); //no se libera ningun objeto
-        p1 = null;//se libera la memoria del objeto referenciado por p1
-        p2 = null;//se libera la memoria del objeto referenciado por p2
-        d1 = null;//se libera la memoria del objeto referenciado por d1
-        d2 = null;
-        System.gc(); //se libera la memoria del objeto referenciado por p1
+        p1=new Prueba();//se libera la referencia p1
+        System.gc(); //No se libera ningun objeto
+        p1=null;//se libera la referencia p1
+        d1=new Demo();
+        System.gc(); //Se libera dos objetos: p1 y d1
+        //debido a que el recolector de basura no se ejecuta inmediatamente
+        //se espera un segundo para que se ejecute
+        Thread.sleep(1000); // Esperar un segundo para dar tiempo al recolector de basura
+
+        
 
 
 
