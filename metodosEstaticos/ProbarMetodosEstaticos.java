eb985a2ad03d2fa374302d56259397f1fef25bf0
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-10-09 17:05:24 +0000

    pendinte metodo estatico

---
+++ metodosEstaticos/ProbarMetodosEstaticos.java
@@ -0,0 +1,14 @@
+package metodosEstaticos;
+
+public class ProbarMetodosEstaticos {
+    public static void main(String[] args) {
+        SumaSerie obj = new SumaSerie();
+        
+        
+        
+    }
+    
+
+    }
+    
+}
--- metodosEstaticos/SumaSerie.java
+++ metodosEstaticos/SumaSerie.java
@@ -4,9 +4,11 @@ public class SumaSerie {
     private static long n;
     private static long m;
     static{
+    
         n = 0;
         m= 1;
     }
+
     public static long suma(){
         m = m +n;
         n = m-n;
